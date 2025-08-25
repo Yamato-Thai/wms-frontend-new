271b1e7cd13fbaafa1d09befd68e8024d413b162
ApoRaviz <tanonchai.pr@hotmail.com>
2025-08-25 14:05:45 +0700

    add component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,9 +8,18 @@ export const routes: Routes = [
 		component: MainLayoutComponent,
 		children: [
 			{
+				path: '',
+				redirectTo: 'dashboard',  // ถ้าเข้าหน้า '' ให้ไป dashboard
+				pathMatch: 'full'
+			},
+			{
+				path: 'dashboard',
+				loadChildren: () => import('./dashboard/dashboard.routes').then(m => m.routes)
+			},
+			{
 				path: 'inbound',
 				loadChildren: () => import('./inbound/inbound.routes').then(m => m.routes)
-			}
+			},
 			// เพิ่ม feature อื่น ๆ ที่ต้องการอยู่ใน layout นี้
 		]
 	}
