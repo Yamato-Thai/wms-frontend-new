9f22d32cef8c13759e2470fd19accc970bee621b
ApoRaviz <tanonchai.pr@hotmail.com>
2025-08-25 12:01:28 +0700

    install tailwind css

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,17 @@
 
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './layout/main-layout.component';
 
 export const routes: Routes = [
 	{
-		path: 'inbound',
-		loadChildren: () => import('./inbound/inbound.routes').then(m => m.routes)
+		path: '',
+		component: MainLayoutComponent,
+		children: [
+			{
+				path: 'inbound',
+				loadChildren: () => import('./inbound/inbound.routes').then(m => m.routes)
+			}
+			// เพิ่ม feature อื่น ๆ ที่ต้องการอยู่ใน layout นี้
+		]
 	}
 ];
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts,scss,sass,css}",
+    "./projects/**/*.{html,ts,scss,sass,css}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
