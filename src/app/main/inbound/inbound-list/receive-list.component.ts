1e09df567b9dd7df859cb288c99668336649ce85
ApoRaviz <130882268+ApoRaviz@users.noreply.github.com>
2025-09-02 12:48:03 +0700

    add css

--- src/app/main/inbound/inbound-list/receive-list.component.ts
+++ src/app/main/inbound/inbound-list/receive-list.component.ts
@@ -1,10 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-receive-list',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './receive-list.component.html',
   styleUrls: ['./receive-list.component.scss']
 })
-export class ReceiveListComponent {}
-console.log('ReceiveListComponent loaded');
+export class ReceiveListComponent {
+ toggleState1 = true;
+  toggleState2 = false;
+  darkMode = false;
+  notifications = true;
+  showDropdown = false;
+  showModal = false;
+  showConfirmModal = false;
+  showImageModal = false;
+  showSuccessToast = false;
+  showWarningToast = false;
+  showErrorToast = false;
+  showTooltip1 = false;
+  showTooltip2 = false;
+}
+
